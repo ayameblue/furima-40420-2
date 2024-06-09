46f74944a63de360bbfd5433457372c8e832ddc9
ayameblue <0af72-3.325635p@ezweb.ne.jp>
2024-06-09 16:39:23 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :nickname, presence: true, uniqueness: true
-    validates :email, presence: true,   uniqueness: true
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :nickname, presence: true
+    validates :email, presence: true
+    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday, presence: true
+    validates :password, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
